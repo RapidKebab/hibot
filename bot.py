b1ed2aaff05dc8ae28aaab116c85edf4749db161
RapidKebab <56969619+RapidKebab@users.noreply.github.com>
2019-10-24 17:47:39 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+# bot.py
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+	rn = random.randint(1,20)
+	print(rn)
+	if rn<6:
+		await message.channel.send('hi')
+    
+
+client.run(token)
